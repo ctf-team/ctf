afb2900d5e492fb399ad39c235c8b1448e0f48e4
ThrDev <admin@thrdu.de>
2018-11-12 18:43:29 -0500

    add error code

--- cmd/web/dropzone/www/upload.php
+++ cmd/web/dropzone/www/upload.php
@@ -4,5 +4,6 @@ if(in_array($mimetype, array('image/jpeg', 'image/gif', 'image/png'))) {
     move_uploaded_file($_FILES['file']['tmp_name'], dirname(__FILE__).'/uploads/' . $_FILES['file']['name']);
     echo "Uploaded successfully!";
 } else {
+    http_response_code(403);
     echo "Please upload a real image";
 }
