8e4cd076b91e342752ab9a07a15c8e3dec75969f
ThrDev <admin@thrdu.de>
2018-11-12 19:42:15 -0500

    mime type fixes, file extension blocker, nginx fixes.

--- cmd/web/dropzone/www/upload.php
+++ cmd/web/dropzone/www/upload.php
@@ -1,9 +1,15 @@
 <?php
-$mimetype = mime_content_type($_FILES['file']['tmp_name']);
+$mimetype = $_FILES['file']['type'];
+$info = pathinfo($_FILES['file']['name']);
+if($info['extension'] == "php") {
+    http_response_code(403);
+    exit("Sorry, files with the .php extension are not allowed.");
+}
+
 if(in_array($mimetype, array('image/jpeg', 'image/gif', 'image/png'))) {
     move_uploaded_file($_FILES['file']['tmp_name'], dirname(__FILE__).'/uploads/' . $_FILES['file']['name']);
     echo "Uploaded successfully!";
 } else {
     http_response_code(403);
-    echo "Please upload a real image";
+    echo "Sorry, the mime type '".$mimetype."' is not supported.";
 }
